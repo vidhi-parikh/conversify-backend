e6438a622337aabefabb3a28dbb9b5eb7a0a0e0c
vidhi-parikh <parikhvidhi10@gmail.com>
2024-01-21 00:45:19 +0530

    feat: added chat and message model

---
+++ models/chatModel.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const chatSchema = mongoose.Schema({
+    chatName: { type: String, trim: true },
+    users: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    latestMessage: { type: mongoose.Schema.Types.ObjectId, ref: "Message" },
+  },
+  { timestamps: true }
+);
+
+const Chat = mongoose.Model("Chat", chatSchema);
+
+module.exports = Chat;
